0dbdbd662195651488dd55a0f9dad3deb45633f1
Databunker <buggyman026@gmail.com>
2019-06-04 22:09:19 +0530

    changes

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,12 @@
+// basic to display the code with material widgetApp widget in parent app.
+
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(MaterialApp(
+      home: Center(
+          child: Text("Hello World")
+      )
+  ));
+}
